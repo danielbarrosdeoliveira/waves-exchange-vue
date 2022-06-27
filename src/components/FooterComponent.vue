ce8dcca36b39829c2a4a07303bac75baf5202c10
danielbarrosdeoliveira <danielbarrosdeoliveira@outlook.com>
2022-06-27 01:09:52 -0300

    :sparkles: created and use footer component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <footer class="footer">
+    <p>
+      <span id="actual-year"></span>
+      &nbsp;-&nbsp;
+      <a href="https://github.com/danielbarrosdeoliveira" target="_blank"> Daniel Oliveira </a>
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent'
+}
+</script>
+
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  height: 4rem;
+  background: var(--background);
+  width: 100%;
+  color: var(--white);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.footer p {
+  display: flex;
+  justify-content: center;
+}
+
+.footer a {
+  color: var(--white);
+  transition: filter 0.4s;
+}
+
+.footer a:hover {
+  filter: opacity(0.7);
+}
+</style>
